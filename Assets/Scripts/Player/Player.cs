6739aaa849f7072d1da7c629e6fa4405bb18d052
Sami <sami-la98@hotmail.fr>
2024-03-07 15:16:26 +0100

    initial commit

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public Image image;
+    public float health;
+    public float maxHealth;
+    public GameObject[] enemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(2) && GameObject.FindGameObjectWithTag("enemies"))
+        {
+            TakeDamage(10);
+            Debug.Log(health);
+        }
+        //image.fillAmount = health / maxHealth;
+    }
+
+    private void TakeDamage(float damage)
+    {
+        health -= damage;
+        health = health - damage;
+    }
+    
+    
+     
+}
